d7304d3781dff1ceb766309b30b50859d5bc9113
worldlead <130297253+worldlead@users.noreply.github.com>
2023-07-14 00:35:00 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,42 @@
-import '@/styles/globals.css'
+import 'styles/globals.scss'
+import 'styles/cmdk/raycast.scss'
+
+
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
+import { NextSeo } from 'next-seo'
+import Head from 'next/head'
+
+const title = '⌘K'
+const description = 'Fast, composable, unstyled command menu for React'
+const siteUrl = 'https://cmdk.paco.me'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <NextSeo
+        title={`${description} — ${title}`}
+        description={description}
+        openGraph={{
+          type: 'website',
+          url: siteUrl,
+          title,
+          description: description + '.',
+          images: [
+            {
+              url: `${siteUrl}/og.png`,
+              alt: title,
+            },
+          ],
+        }}
+      />
+      <ThemeProvider disableTransitionOnChange attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+/* eslint-disable @next/next/no-sync-scripts */
+import React from 'react'
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preload" href="/inter-var-latin.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
